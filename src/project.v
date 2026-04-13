05b40b7b6740dcc14a02c873ff469902db307539
Sushma1012 <sushmaboorja@gmail.com>
2026-04-14 00:59:36 +0530

    Fix: add wrapper project.v

--- src/project.v
+++ src/project.v
@@ -25,3 +25,28 @@ module tt_um_example (
   wire _unused = &{ena, clk, rst_n, 1'b0};
 
 endmodule
+`default_nettype none
+
+module project (
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
+);
+
+    tt_um_lfsr_prng u_tt_um_lfsr_prng (
+        .ui_in(ui_in),
+        .uo_out(uo_out),
+        .uio_in(uio_in),
+        .uio_out(uio_out),
+        .uio_oe(uio_oe),
+        .ena(ena),
+        .clk(clk),
+        .rst_n(rst_n)
+    );
+
+endmodule
